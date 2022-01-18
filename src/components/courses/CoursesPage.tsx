04b28e4651081804ed509631f82bcdbeae46e5ed
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-01-18 11:47:43 +0100

    [ADD] basic course add form

--- src/components/courses/CoursesPage.tsx
+++ src/components/courses/CoursesPage.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 const CoursesPage = () => {
-    return (
+
+    const [course, changeCourse] = useState({title: ''})
+
+    const handleChange = (event : any) => {
+        changeCourse({...course, title: event.target.value})
+    }
+
+    return (<div>
         <h2>Courses Page</h2>
+        <h3>Add course</h3>
+        <input
+        type="text"
+        placeholder="add course name"
+        value={course.title}
+        onChange={(event) => handleChange(event)}
+        />
+        <input type='submit' value='save'/>
+        </div>
     )
 }
 
