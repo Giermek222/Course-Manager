673606f0a596e6ef17195ef1e2bb0b497c877fd4
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-01-21 01:30:41 +0100

    [ADD] mock API

--- src/components/courses/CoursesPage.tsx
+++ src/components/courses/CoursesPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+    import { useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import createCourse from "../../redux/actions/courseActions";
